f970cf4a5a2ac81cba518293a4cc10aed710239d
Jonalyn Mobilla <JONALYN@MLHUILLIER.LOCAL>
2021-04-20 16:02:43 +0800

    second commit

--- app/Http/Controllers/SendEmailController.php
+++ app/Http/Controllers/SendEmailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use App\Models\sendEmail;
 use Illuminate\Support\Facades\Mail;
 
@@ -10,30 +11,40 @@ class SendEmailController extends Controller
 {
     public function index(Request $request)
     {
-        $request->validate([
-            'topic' => 'required | string |max: 25',
-            'email' => 'required | email | string',
-            'body' => 'required | string | max: 255'
-        ]);
+
         $data = [
             'topic' => $request->topic,
             'email' => $request->email,
             'body' => $request->body
       ];
- 
+
+      
+
       try {
+
+        $request->validate([
+            'topic' => 'required | string |max: 25',
+            'email' => 'required | email | string |unique:users|ends_with:mlhuillier.com',
+            'body' => 'required | string | max: 255'
+        ]);
+
         Mail::send([], $data, function($message)use($data) {
             $message->to($data["email"], $data["email"])
                     ->subject($data["topic"])
                     ->setBody($data["body"]);
-                    
-                    
+   
         });
-        return response()->json($data);
-        echo"test";
-        dd('Mail sent successfully');
-      } catch (Exception $e) {
-         return response()->json($e);
+        return response()->json([
+            'message' => 'Success',
+            'data' => $data
+        ]);
+        
+      } catch (ValidationException $e) {
+         return response()->json([
+             'errors' =>$e->errors(),
+             'message' => 'Failed'
+         ]);
+         
       }
        
     }
