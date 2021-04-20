6b59e7441b1ce4447123d119c9ea4be3fef57d78
Jonalyn Mobilla <JONALYN@MLHUILLIER.LOCAL>
2021-04-20 08:07:16 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="#000000"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://4.bp.blogspot.com/-yYrR3xd7428/W6n1_nO_hMI/AAAAAAAAF9Y/KRxAgHldpD4_fdlJwAaVuvjQIczOZxzOACLcBGAs/s1600/3.png"
+          transition="scale-transition"
+          width="40"
+        />
+        <h3>ML-Project</h3>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        text
+      >
+        <span class="mr-2">Logout</span>
+      </v-btn>
+      <template v-slot:extension>
+        <v-tabs color="white" background-color="#B71C1C">
+          <v-tab>Manage request</v-tab>
+          <v-tab>Manage content</v-tab>
+          <v-tab>Reports</v-tab>
+        <v-tab>Manage users</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+
+
+    <v-main>
+      <Email/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Email from './components/Email';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Email,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Email.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container pt-5">
+    <br /><br /><br />
+    <div class="card p-5">
+      <h4>Email Our Mailing List</h4>
+      <small class="pb-3"
+        >Sending bulk email to our mailing list contacts using Twilio
+        SendGrid</small
+      >
+      <form v-on:submit.prevent="send">
+        <div class="form-group">
+          <label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Topic"
+              v-model="data.topic"
+            />
+          </label>
+        </div>
+        <div class="form-group">
+          <label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Sender Email"
+              v-model="data.email"
+            />
+          </label>
+        </div>
+        <div class="form-group">
+          <label>
+            <textarea
+              class="form-control"
+              placeholder="Enter email body"
+              v-model="data.body"
+            ></textarea>
+          </label>
+        </div>
+        <button type="submit" class="btn btn-outline-success">
+          Send Emails
+        </button>
+      </form>
+      <div class="pt-3 danger">
+        {{ this.error }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      data: {},
+      error: {},
+    };
+  },
+  mounted(){
+    console.log('component mounted')
+  },
+  methods: {
+    send() {
+      console.log('sdsfdgfd');
+        axios.post("http://127.0.0.1:8000/api/send/email", this.data)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((error) => {
+          console.log(error);
+          alert(this.data.errors);
+        });
+    },
+  },
+};
+</script>
